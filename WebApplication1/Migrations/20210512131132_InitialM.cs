047e9a79b688edcd7bcfba910bd912379f67c3d3
Zynno-Dev <michelinbriandev@gmail.com>
2021-05-12 12:47:54 -0300

    first commit

---
+++ WebApplication1/Migrations/20210512131132_InitialM.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApplication1.Migrations
+{
+    public partial class InitialM : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TablaUno",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(maxLength: 10, nullable: false),
+                    Lastname = table.Column<string>(maxLength: 10, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TablaUno", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TablaUno");
+        }
+    }
+}
---
+++ WebApplication1/Models/TablaUno.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Data
+{
+    public class TablaUno
+    {   
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage ="El nombre es un campo requerido")]
+        [MaxLength(10, ErrorMessage = "El largo maximo es 10"), MinLength(2, ErrorMessage = "El largo minimo es 2")]
+        public string Name { get; set; }
+        [Required(ErrorMessage ="El apellido es un campo requerido")]
+        [MaxLength(10, ErrorMessage ="El largo maximo es 10"), MinLength(2, ErrorMessage ="El largo minimo es 2")]
+        public string Lastname { get; set; }
+    }
+}
